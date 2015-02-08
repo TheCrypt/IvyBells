0cf23d8e4b89dc06799fccfaf8ee74ff62873cec
Nicolas Faurie <nfaurie.42@gmail.com>
2015-02-08 15:46:59 +0100

    Layer are now rendered in the right order

--- src/Game.js
+++ src/Game.js
@@ -28,6 +28,8 @@ var Game = (function () {
         this.layerCollider = this.map.createLayer('Collider'); // 48 == blocked
         this.layerCollider.visible = false;
 
+        this.group = phaser.add.group(undefined, "General");
+
         this.player = phaser.add.sprite(800, 900, 'player', 1);
         this.player.animations.add('left', [8,9], 10, true);
         this.player.animations.add('right', [1,2], 10, true);
@@ -38,12 +40,18 @@ var Game = (function () {
 
         this.player.body.setSize(10, 14, 2, 1);
 
+        // Render order
+        this.unitGroup.add(this.layer1);
+        this.unitGroup.add(this.player);
+        this.unitGroup.add(this.layer2);
+
+
+
         var previous_mouse_pos = null;
 
         var mouseMovedWhileClicked = false;
         phaser.input.mouse.callbackContext = this;
         phaser.input.mouse.mouseMoveCallback = function (evt) {
-            console.log(evt.x, evt.y);
             if (phaser.input.mousePointer.isUp)
                 previous_mouse_pos = null;
             if (phaser.input.mousePointer.isDown) {
