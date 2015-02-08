1ee1429058ce6b1d6b6dd91e705b86e55eef668a
Massou31 <massou@github>
2015-02-08 13:53:14 +0100

    added camera entity

---
+++ src/entities/EntityCamera.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Massou on 08/02/15.
+ */
+var EntityCamera = (function () {
+
+    function EntityCamera() {
+        this.components = {};
+    }
+
+    return EntityCamera;
+})();
